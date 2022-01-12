0aa383b033fd7f8458af590e2654c19d096a9f1f
mysterioxx <mysteriorohilla@gmail.com>
2022-01-12 10:33:22 +0530

    Update internetspeed.py

--- internetspeed.py
+++ internetspeed.py
@@ -6,3 +6,13 @@ option = int(input('''What speed do you want to test:
 2) Upload Speed
 3) Ping
 Your Choice: '''))
+if option == 1:
+	print(st.download())
+elif option == 2:
+	print(st.upload())
+elif option == 3:
+	servernames =[]
+	st.get_servers(servernames)
+	print(st.results.ping)
+else:
+	print("Please enter the correct choice !")
